5e42ff6e39e9eeedba9fc503e0ac31abb60d18e4
YoungRok Son <youngrokson@YoungRoks-MacBook-Pro.local>
2022-05-04 16:36:38 +0900

    generate data init.

---
+++ data_sets/generate_data_sets.cpp
@@ -0,0 +1,5 @@
+#include "generate_data_sets.hpp"
+
+int main()
+{
+    std::cout << "Data has generated." << std::endl;
---
+++ data_sets/generate_data_sets.hpp
@@ -0,0 +1,10 @@
+#ifndef __GENERATE_DATA_SETS_HPP__
+#define __GENERATE_DATA_SETS_HPP__ 
+
+
+// Import Standard Libraries
+#include <iostream>
+
+// Import Open3D libraries
+
+#endif
